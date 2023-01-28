639a2f5d45020c87f6d38cfc249311552ee9d2dc
Renzo Posobon <renzoposobon95@gmail.com>
2023-01-28 17:09:05 -0300

    responsive cart

--- src/componentes/ProductCart.jsx
+++ src/componentes/ProductCart.jsx
@@ -8,18 +8,18 @@ const ProductCart = () => {
 
 
   return (
-    <div className="m-10 text-xl font">
+    <div className="m-10 text-xl font" id="productCart">
       { items.length === 0 ?
         <div>
-          <h1 className="text-3xl font-bold m-10 m">Carrito vacío 🤔</h1>
+          <h1 className="text-3xl font-bold m-10 m titleCart">Carrito vacío 🤔</h1>
           <p className="text-xl font-semibold m-10 m">¡Agrega un producto a tu carrito!</p>
           <Link to='/products'>
-            <button className="btn text-base font-semibold btn-sm py-1 px-4 text-white bg-black rounded-none ml-10 h-14">Ir a Productos</button>
+            <button className="btn text-base font-semibold btn-sm py-1 px-4 text-white bg-black rounded-none ml-10 h-14 buttonProuctCart">Ir a Productos</button>
           </Link>
         </div>
         :
         <div>
-          <h1 className="text-3xl font-bold mb-6">Tus productos:</h1>
+          <h1 className="text-3xl font-bold mb-6 titleCart mt-4">Tus productos:</h1>
           <ContainerCart />
         </div>
       }
